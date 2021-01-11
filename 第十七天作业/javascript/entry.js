bc111fdaf437b15e6cf5e1b1a91b6010a64a1f2d
shaowei <3578151718@qq.com>
2021-01-11 15:22:55 +0800

    1

---
+++ 第十七天作业/javascript/entry.js
@@ -0,0 +1,10 @@
+//  引入需要打包的js文件
+
+require('./xiaobaibai');
+require('./xubin');
+require('./zhangfan');
+require('../css/index.css');
+
+//  如果我们需要引入bootstrap，那么需要在这里进行引入
+//  只需要引入bootstrap的入口文件，就能引入所有的less
+require('../less/bootstrap.less');
